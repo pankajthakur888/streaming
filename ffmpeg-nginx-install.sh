652b33d34604b34bfbc3fa60000b9cd3f0ebf681
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2021-01-10 23:15:09 +0530

    Update ffmpeg-nginx-install.sh

--- ffmpeg-nginx-install.sh
+++ ffmpeg-nginx-install.sh
@@ -15,26 +15,35 @@
 	cd /opt/ffmpeg
 	sudo wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
 
-#Extract the static build from the archive.
+##Extract the static build from the archive.
 	sudo tar xvf ffmpeg*.xz
 	cd ffmpeg-*-static
 	ls
 
-#Install the binaries globally.
+##Install the binaries globally.
 	sudo ln -s "${PWD}/ffmpeg" /usr/local/bin/
 	sudo ln -s "${PWD}/ffprobe" /usr/local/bin/
 
-#=======================================================================================
+#=====================================================================================================#
 
-#Install Nginx 
+#Install Nginx with RTMP Module.
 	sudo mkdir -p /opt/nginx
 	cd /opt/nginx
-	#sudo wget http://nginx.org/download/nginx-1.19.6.zip
+	
+##Download nginx-1.19.6
 	sudo wget https://github.com/pankajthakur888/streaming/raw/main/nginx-1.19.6.zip
 	sudo unzip nginx-1.19.6.zip
+	
+##Download nginx-rtmp module
 	sudo wget https://github.com/pankajthakur888/streaming/raw/main/nginx-rtmp.zip
 	sudo unzip nginx-rtmp.zip
+	
+##Compile with Nginx with RTMP Module
 	cd nginx-1.19.6
 	sudo ./configure --prefix=/usr/local/nginx --with-http_ssl_module --add-module=../nginx-rtmp-module
 	sudo make
 	sudo make install
+	
+	sudo mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf-old
+	sudo cd /usr/local/nginx/conf/
+	sudo wget https://github.com/pankajthakur888/streaming/raw/main/nginx.conf

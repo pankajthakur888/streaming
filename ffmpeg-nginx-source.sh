a028b02e2af59178471e6e8ca73eef0cf18b84a9
Pankaj Kumar <45850188+pankajthakur888@users.noreply.github.com>
2021-01-15 11:22:09 +0530

    Update ffmpeg-nginx-source.sh

--- ffmpeg-nginx-source.sh
+++ ffmpeg-nginx-source.sh
@@ -66,29 +66,28 @@
 	man nginx
 
 #Compile with Nginx with RTMP Module
-	cd nginx-1.19.6
+	cd nginx-1.19.0
 	
 	#sudo ./configure --prefix=/usr/local/nginx --with-http_ssl_module --with-http_secure_link_module --add-module=../nginx-rtmp-module
-./configure --prefix=/usr/local/nginx \
-	--sbin-path=/usr/local/nginx/sbin/nginx \
-	--conf-path=/usr/local/nginx/conf/nginx.conf \
-	--pid-path=/usr/local/nginx/pid/nginx.pid \
-	--with-mail_ssl_module \
-	--with-stream=dynamic \
-	--with-stream_ssl_module \
-	--with-stream_realip_module \
-	--with-stream_geoip_module=dynamic \
-	--with-stream_ssl_preread_module \
-	--with-compat \
-	--with-http_ssl_module \
-	--with-pcre=../pcre-8.42 \
-	--with-pcre-jit \
-	--with-zlib=../zlib-1.2.11 \
-	--with-openssl=../openssl-1.1.0h \
-	--without-http_empty_gif_module \
-	--with-openssl-opt=no-nextprotoneg \
-	--with-http_secure_link_module \
-	--add-module=../nginx-rtmp-module
+#./configure --prefix=/usr/local/nginx \
+#	--sbin-path=/usr/local/nginx/sbin/nginx \
+#	--conf-path=/usr/local/nginx/conf/nginx.conf \
+#	--pid-path=/usr/local/nginx/pid/nginx.pid \
+#	--with-mail_ssl_module \
+#	--with-stream=dynamic \
+#	--with-stream_ssl_module \
+#	--with-stream_geoip_module=dynamic \
+#	--with-stream_ssl_preread_module \
+#	--with-compat \
+#	--with-http_ssl_module \
+#	--with-pcre=../pcre-8.42 \
+#	--with-pcre-jit \
+#	--with-zlib=../zlib-1.2.11 \
+#	--with-openssl=../openssl-1.1.0h \
+#	--without-http_empty_gif_module \
+#	--with-openssl-opt=no-nextprotoneg \
+#	--with-http_secure_link_module \
+#	--add-module=../nginx-rtmp-module
 	
 ##WITH USER
 ./configure --prefix=/usr/local/nginx \
@@ -115,62 +114,59 @@
 	--with-http_secure_link_module \
 	--add-module=../nginx-rtmp-module
 	
-./configure --prefix=/usr/local/nginx \
-	--sbin-path=/usr/local/nginx/sbin/nginx \
-	--conf-path=/usr/local/nginx/conf/nginx.conf \
-	--pid-path=/usr/local/nginx/pid/nginx.pid \
-	--modules-path=/usr/lib/nginx/modules \ 
-	--user=nginx \
-	--group=nginx \
-	--build=Ubuntu \
-	--builddir=nginx-1.19.0 \
-	--with-select_module \
-	--with-poll_module \
-	--with-threads \
-	--with-file-aio \
-	--with-http_ssl_module \
-	--with-http_v2_module \
-	--with-http_realip_module \
-	--with-http_addition_module \
-	--with-http_xslt_module=dynamic \
-	--with-http_image_filter_module=dynamic \
-	--with-http_geoip_module=dynamic \
-	--with-http_sub_module \
-	--with-http_dav_module \
-	--with-http_flv_module \
-	--with-http_mp4_module \
-	--with-http_gunzip_module \
-	--with-http_gzip_static_module \
-	--with-http_auth_request_module \
-	--with-http_random_index_module \
-	--with-http_secure_link_module \
-	--with-http_degradation_module \
-	--with-http_slice_module \
-	--with-http_stub_status_module \
-	--with-http_perl_module=dynamic \
-	--with-perl_modules_path=/usr/share/perl/5.26.1 \
-	--with-perl=/usr/bin/perl \
-	--http-log-path=/var/log/nginx/access.log \
-	--http-client-body-temp-path=/var/cache/nginx/client_temp \
-	--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
-	--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
-	--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
-	--http-scgi-temp-path=/var/cache/nginx/scgi_temp \
-	--with-mail=dynamic \
-	--with-mail_ssl_module \
-	--with-stream=dynamic \
-	--with-stream_ssl_module \
-	--with-stream_realip_module \
-	--with-stream_geoip_module=dynamic \
-	--with-stream_ssl_preread_module \
-	--with-compat \
-	--with-pcre=../pcre-8.42 \
-	--with-pcre-jit \
-	--with-zlib=../zlib-1.2.11 \
-	--with-openssl=../openssl-1.1.0h \
-	--with-openssl-opt=no-nextprotoneg \
-	--with-debug \
-	--add-module=../nginx-rtmp-module
+#./configure --prefix=/usr/local/nginx \
+#	--sbin-path=/usr/local/nginx/sbin/nginx \
+#	--conf-path=/usr/local/nginx/conf/nginx.conf \
+#	--pid-path=/usr/local/nginx/pid/nginx.pid \
+#	--user=nginx \
+#	--group=nginx \
+#	--build=Ubuntu \
+#	--builddir=nginx-1.19.0 \
+#	--with-poll_module \
+#	--with-threads \
+#	--with-file-aio \
+#	--with-http_ssl_module \
+#	--with-http_v2_module \
+#	--with-http_realip_module \
+#	--with-http_addition_module \
+#	--with-http_xslt_module=dynamic \
+#	--with-http_image_filter_module=dynamic \
+#	--with-http_geoip_module=dynamic \
+#	--with-http_dav_module \
+#	--with-http_flv_module \
+#	--with-http_mp4_module \
+#	--with-http_gunzip_module \
+#	--with-http_gzip_static_module \
+#	--with-http_auth_request_module \
+#	--with-http_random_index_module \
+#	--with-http_secure_link_module \
+#	--with-http_degradation_module \
+#	--with-http_slice_module \
+#	--with-http_stub_status_module \
+#	--with-http_perl_module=dynamic \
+#	--with-perl_modules_path=/usr/share/perl/5.26.1 \
+#	--with-perl=/usr/bin/perl \
+#	--http-log-path=/var/log/nginx/access.log \
+#	--http-client-body-temp-path=/var/cache/nginx/client_temp \
+#	--http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+#	--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+#	--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+#	--http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+#	--with-mail=dynamic \
+#	--with-mail_ssl_module \
+#	--with-stream=dynamic \
+#	--with-stream_ssl_module \
+#	--with-stream_realip_module \
+#	--with-stream_geoip_module=dynamic \
+#	--with-stream_ssl_preread_module \
+#	--with-compat \
+#	--with-pcre=../pcre-8.42 \
+#	--with-pcre-jit \
+#	--with-zlib=../zlib-1.2.11 \
+#	--with-openssl=../openssl-1.1.0h \
+#	--with-openssl-opt=no-nextprotoneg \
+#	--with-debug \
+#	--add-module=../nginx-rtmp-module
 
 	make
 	sudo make install
